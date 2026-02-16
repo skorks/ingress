f6a4999f7a530f6caf8c1665ecd08cd117a71876
Alan Skorkin <109413+skorks@users.noreply.github.com>
2026-02-16 12:24:53 +1100

    feat: add modern RSpec configuration with SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,30 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'ingress'
+# frozen_string_literal: true
+
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+require "ingress"
+
+RSpec.configure do |config|
+  # Modern expect syntax only
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+    expectations.syntax = :expect
+  end
+
+  # Modern mock syntax
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  # Better output and test management
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+  config.warnings = true
+  config.order = :random
+  Kernel.srand config.seed
+end
